cac916e5427406d4e05b744a07a5553c3df05a0f
Diogo Freitas <diogo.freitas@hotmail.com>
2020-01-06 21:32:31 -0300

    Adicionar arquivos de projeto.

---
+++ ConsultaCep/ConsultaCep/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ConsultaCep.Service;
+using ConsultaCep.Service.Model;
+
+namespace ConsultaCep
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnBuscar.Clicked += BuscarCep;
+        }
+
+        private void BuscarCep(object sender, EventArgs args)
+        {
+            string cep = txtName.Text.Trim();
+            if (isValidCep(cep))
+            {
+                try
+                {
+                    Address end = ViaCepService.SearchAddressViaCep(cep);
+                    if (end != null)
+                    {
+                        lbResultado.Text = string.Format("Endereço: {0}, {1}, {2}-{3}", end.logradouro, end.bairro, end.localidade, end.uf);
+                    }
+                    else
+                    {
+                        DisplayAlert("Erro", "O Endereço não foi encontrado para o cep encontrado: " + cep, "OK");
+                    }
+                }
+                catch (Exception e)
+                {
+                    DisplayAlert("Erro Critico", e.Message, "OK");
+                }
+            }
+        }
+        private bool isValidCep(string cep)
+        {
+            bool valido = true;
+            if (cep.Length != 8)
+            {
+                DisplayAlert("ERRO", "Cep Invalido! O cep deve conter 8 caracteres.", "OK");
+                valido = false;
+            }
+            int novoCep = 0;
+            if (!int.TryParse(cep, out novoCep))
+            {
+                DisplayAlert("ERRO", "Cep Invalido! O cep deve ser composto somente por numeros.", "OK");
+                valido = false;
+            }
+            return valido;
+        }
+    }
+}
---
+++ ConsultaCep/ConsultaCep/Service/ViaCepService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using ConsultaCep.Service.Model;
+using Newtonsoft.Json;
+
+namespace ConsultaCep.Service
+{
+    class ViaCepService
+    {
+        private static string addressUrl = "https://viacep.com.br/ws/{0}/json/";
+
+        public static Address SearchAddressViaCep(string cep)
+        {
+            string newAddressUrl = string.Format(addressUrl, cep);
+            WebClient wc = new WebClient();
+            string conteudo = wc.DownloadString(newAddressUrl);
+
+            Address end = JsonConvert.DeserializeObject<Address>(conteudo);
+            if (end.cep == null) return null;
+            return end;
+        }
+    }
+}
